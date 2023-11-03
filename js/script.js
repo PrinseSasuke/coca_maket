b683e94595d7ef9cebc8ad40d488a3dde8660a64
PrinseSasuke <sergey.pirkov@mail.ru>
2023-11-03 21:58:23 +0300

    carusel draggable

--- js/script.js
+++ js/script.js
@@ -59,17 +59,22 @@ const carousel = document.querySelector(".slider-list");
 let isDragStart = false, prevPageX, prevScrollLeft;
 const dragStart = (e) => {
     isDragStart = true;
-    prevPageX = e.pageX;
+    prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 const dragging = (e) => {
     if (!isDragStart) return;
-    let positionDiff = e.pageX - prevPageX;
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
 }
 const dragEnd = () =>{
     isDragStart = false;
 }
 carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
 carousel.addEventListener("mouseup", dragEnd);
+carousel.addEventListener("touchend", dragEnd);
